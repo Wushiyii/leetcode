f3e1104ba9c701e78c143cdf39453f7973d6a4e2
jessyy <wgq287676521@qq.com>
2018-12-15 00:48:28 +0800

    增加Q150每日温度

---
+++ src/question/Q150_Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,46 @@
+package question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Q150_Evaluate_Reverse_Polish_Notation {
+
+
+    public int evalRPN(String[] tokens) {
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
+                int a = Integer.parseInt(stack.pop());
+                int b = Integer.parseInt(stack.pop());
+                switch (tokens[i])
+                {
+                    case "+":
+                        int c = b + a;
+                        stack.push(String.valueOf(c));
+                        break;
+                    case "-":
+                        c = b - a;
+                        stack.push(String.valueOf(c));
+                        break;
+                    case "*":
+                        c = b * a;
+                        stack.push(String.valueOf(c));
+                        break;
+                    case "/":
+                        c = b / a;
+                        stack.push(String.valueOf(c));
+                        break;
+
+
+                }
+            }else {
+                stack.push(tokens[i]);
+            }
+
+        }
+        return Integer.parseInt(stack.peek());
+    }
+
+}
