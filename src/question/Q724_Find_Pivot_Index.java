cac93cbf75a480e425c04585b784a9454ffb6004
jessyy <wgq287676521@qq.com>
2018-12-21 00:16:10 +0800

    增加两种优化解

--- src/question/Q724_Find_Pivot_Index.java
+++ src/question/Q724_Find_Pivot_Index.java
@@ -2,8 +2,45 @@ package question;
 
 public class Q724_Find_Pivot_Index {
 
+
+    //左增右减实现
     public int pivotIndex(int[] nums) {
-        int res = -1;
+        if(nums.length < 3) return -1;
+        int left=0, right=0;
+        for(int i=1;i<nums.length;i++) {
+            right+=nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if ( left == right) return i;
+            if(i != nums.length-1) {
+                left += nums[i];
+                right -= nums[i+1];
+            }
+        }
+        return -1;
+
+    }
+
+
+    //遍历取一半值
+    public int pivotIndex3(int[] nums) {
+        if(nums.length < 3) return -1;
+        int sum=0 , left=0;
+        for(int i=0;i<nums.length;i++) {
+            sum+=nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if ((float)(sum - nums[i])/2 == left){
+                return i;
+            }else {
+                left += nums[i];
+            }
+        }
+        return -1;
+    }
+
+    //穷举  n^2
+    public int pivotIndex2(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int left= 0;
             int right = 0;
@@ -14,11 +51,10 @@ public class Q724_Find_Pivot_Index {
                 right +=nums[j];
             }
             if (left == right) {
-                res = i;
-                break;
+                return i;
             }
         }
-        return res;
+        return -1;
     }
 
     public static void main(String[] args) {
