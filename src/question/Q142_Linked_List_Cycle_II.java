829f31e003f31705e0a715cbcbf44245b90f30ca
jessyy <wgq287676521@qq.com>
2019-01-13 23:26:56 +0800

    add

--- src/question/Q142_Linked_List_Cycle_II.java
+++ src/question/Q142_Linked_List_Cycle_II.java
@@ -5,7 +5,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Q142_Linked_List_Cycle_II {
-    public ListNode detectCycle(ListNode head) {
+    //hash
+    public ListNode detectCycle1(ListNode head) {
+        if (head == null || head.next == null) return null;
         Set<ListNode> set = new HashSet<>();
         while (head != null) {
             if (set.contains(head)){
@@ -16,5 +18,42 @@ public class Q142_Linked_List_Cycle_II {
             head = head.next;
         }
         return null;
+    }
+    //双指针
+    public ListNode detectCycle2(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (true) {
+            if (fast == null || fast.next == null) {
+                return null;
+            } else {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            if (slow == fast) {
+                break;
+            }
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(3);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(0);
+        ListNode d = new ListNode(-4);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = b;
+        Q142_Linked_List_Cycle_II demo = new Q142_Linked_List_Cycle_II();
+        ListNode node = demo.detectCycle2(a);
+        System.out.println(node);
     }
 }
