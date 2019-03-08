f0c60c6968f0b43269373d764bdcc2fa40eaf583
jessyy <wgq287676521@qq.com>
2019-03-08 11:29:44 +0800

    add solution

---
+++ src/question/Q102_Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,31 @@
+package question;
+
+import java.util.*;
+
+public class Q102_Binary_Tree_Level_Order_Traversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (count > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                count--;
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
